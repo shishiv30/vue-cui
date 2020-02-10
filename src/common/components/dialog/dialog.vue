add458641131cc303c5ab852ef5f13d129231c3d
Conjee Zou <shishiv30@live.cn>
2020-02-09 20:38:49 -0800

    update height

---
+++ src/common/components/dialog/dialog.vue
@@ -0,0 +1,10 @@
+<template lang="html">
+    <div class="dialog" :class="{active:active, hoverable:hoverable}">
+        <slot></slot>
+    </div>
+</template>
+<script src="./dialog.js"></script>
+<style lang="scss">
+    @import "./dialog.scss";
+
+</style>
